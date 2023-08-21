fbb0f85b4bd67ace5ae44b045b25746c42b0ac8d
Vivek <vivekvpm1@gmail.com>
2023-08-21 18:16:51 +0000

    fixing errors

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -5,9 +5,3 @@ module "s3_bucket" {
   bucket = "${var.prefix}-s3-bucket"
   acl    = "log-delivery-write"
 }
-
-terraform {
-  required_providers {
-    aws = "=3.42.0" # or anything higher than 4.5.0
-  }
-}
