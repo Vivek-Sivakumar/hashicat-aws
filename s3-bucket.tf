ab4aa268c27b8f9607047fe821b0443f7cdadb7e
Vivek <vivekvpm1@gmail.com>
2023-08-21 18:12:52 +0000

    add s3 bucket using module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,13 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
 
   bucket = "${var.prefix}-s3-bucket"
+  acl    = "log-delivery-write"
+}
 
+terraform {
+  required_providers {
+    aws = ">= =3.42.0" # or anything higher than 4.5.0
+  }
 }
